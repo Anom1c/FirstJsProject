c4aa55234feb010e0795403eb2a1cf87e3a034b4
Danylo Bobrov <danylo.bobrov@globallogic.com>
2021-02-17 11:20:07 +0200

    Snake

--- js/action.js
+++ js/action.js
@@ -1,24 +1,43 @@
 $(document).ready(function () {
     let canvas;
     let ctx;
-    let x;
-    let y;
-    let sideSize = 30;
+    let snakeSize;
     let snakePart = {x: 0, y: 0};
+    let fieldSize ;
+    let direction ;
+    let actionTimeOut;
     console.log(`Window Ready`);
 
     function init() {
         canvas = $("#myCanvas");
         ctx = canvas[0].getContext('2d');
-        x = canvas.width() / 2;
-        y = canvas.height() / 2;
-        snakePart.x = x;
-        snakePart.y = y;
+        fieldSize = canvas.width();
+        snakeSize = fieldSize / 20;
+        snakePart.x = fieldSize / 2;
+        snakePart.y = fieldSize / 2;
+        direction = left;
         drawRect(snakePart);
         window.addEventListener("keydown", update, false);
+        const triggerSnake = document.querySelector("#stopSnake")
+        // triggerSnake.addEventListener("click", run, false);
         render();
+        run();
+
+    }
+    function moveSnakePart(){
+        direction(snakePart);
+        render();
+        checkForBorder(snakePart);
     }
 
+
+    function run(){
+         actionTimeOut = setTimeout(function (){
+            moveSnakePart();
+            run();
+        },250);
+
+    }
     /**
      *
      * @param {{x: number, y: number}}snakePart
@@ -26,7 +45,7 @@ $(document).ready(function () {
     function drawRect(snakePart) {
         console.log('drawRect was called with x - %d, y - %d', snakePart.x, snakePart.y);
         ctx.fillStyle = '#666';
-        ctx.fillRect(snakePart.x, snakePart.y, sideSize, sideSize);
+        ctx.fillRect(snakePart.x, snakePart.y, snakeSize, snakeSize);
     }
 
     $(window).on('load', function () {
@@ -34,30 +53,54 @@ $(document).ready(function () {
         init();
     });
 
-    function
-    update(event) {
+    function update(event) {
         if (event.keyCode == 38) {
-            snakePart.y -= sideSize; //going up
+            direction = up; //going up
         }
         if (event.keyCode == 40) {
-            snakePart.y += sideSize; //going down
+            direction = down; //going down
         }
         if (event.keyCode == 37) {
-            snakePart.x -= sideSize; //going left
+            direction = left; //going left
         }
         if (event.keyCode == 39) {
-            snakePart.x += sideSize; //going right
+            direction = right; //going right
         }
         render();
+
     }
 
     function render() {
         ctx.clearRect(0, 0, 600, 600);
         drawRect(snakePart);
 
-
     }
-    setInterval(function (){
-        snakePart.x -= sideSize;
-    },2000)
+    function checkForBorder(snakePart) {
+        console.log(snakePart.x)
+        if (snakePart.x === fieldSize - snakeSize || snakePart.x === 0 ||
+            snakePart.y === fieldSize - snakeSize || snakePart.y === 0)
+        {
+            clearTimeout(actionTimeOut);
+            return alert("Snake faced with border");
+        }
+    }
+
+
+    function up(snakePart){
+        snakePart.y = snakePart.y - snakeSize ;
+        return snakePart;
+    }
+    function down(snakePart){
+        snakePart.y = snakePart.y + snakeSize
+        return snakePart;
+    }
+    function left(snakePart){
+        snakePart.x = snakePart.x - snakeSize;
+        return snakePart;
+    }
+    function right(snakePart){
+        snakePart.x = snakePart.x + snakeSize;
+        return snakePart;
+    }
+
 });
---
+++ temp.js
@@ -0,0 +1,8 @@
+let a = 5;
+let b = 7;
+let c = 9;
+let d;
+
+d = b;
+a = b;
+d = a;
